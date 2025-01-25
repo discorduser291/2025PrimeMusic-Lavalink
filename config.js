11610f95d9e658400670c4f99fce8428e2825f7f
discorduser291 <sengkoon89@gmail.com>
2025-01-25 22:05:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "/play", 
+  activityName: "/help", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
